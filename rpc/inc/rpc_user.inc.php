9d148b85f2b6961270767b8209d409939dd455c4
Michael Berkowski <mjb@umn.edu>
2014-06-27 21:31:20 -0500

    Changed big ugly (faulty) email validation regex for filter_var()

--- rpc/inc/rpc_user.inc.php
+++ rpc/inc/rpc_user.inc.php
@@ -899,8 +899,7 @@ QRY;
 	 */
 	public static function validate_email($email)
 	{
-		$regex = "/^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i";
-		return preg_match($regex, $email);
+		return (bool)filter_var($email, FILTER_VALIDATE_EMAIL);
 	}
 
 	/**
