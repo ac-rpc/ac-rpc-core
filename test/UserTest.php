ceb9da5f51b4de8200045d4e315094a480c9a278
Michael Berkowski <mjb@umn.edu>
2014-07-05 18:55:55 -0500

    User assignment query tests

--- test/UserTest.php
+++ test/UserTest.php
@@ -21,10 +21,22 @@ class RpcTest extends RPC_PHPUnit_Extensions_Databaase_TestCase
 		$user = new RPC_User(RPC_User::RPC_QUERY_USER_BY_ID, 1, $config, $db);
 		$this->assertEquals('user1newemail@example.com', $user->email);
 		
-		// Should have 1 template
-		$this->assertEquals(1, count(RPC_User::get_templates($user)));
+		// Should have 2 templates
+		$this->assertEquals(2, count(RPC_User::get_templates($user)));
 
-		// Should have 2 assignments
-		$this->assertEquals(1, count($user->get_assignments()));
+		// Total 4
+		// Inactive 1
+		// Active 2
+		// Expired 1
+		$this->assertEquals(4, count($user->get_assignments()));
+		$this->assertEquals(2, count($user->get_assignments(RPC_User::RPC_ASSIGNMENT_STATUS_ACTIVE)));
+		$this->assertEquals(1, count($user->get_assignments(RPC_User::RPC_ASSIGNMENT_STATUS_INACTIVE)));
+		$this->assertEquals(1, count($user->get_assignments(RPC_User::RPC_ASSIGNMENT_STATUS_EXPIRED)));
+
+		// Other user can't see unpublished template
+		$user2 = new RPC_User(RPC_User::RPC_QUERY_USER_BY_USERNAME, 'testuser2@example.com', $config, $db);
+
+		$this->assertEquals(2, $user2->id);
+		$this->assertEquals(1, count(RPC_User::get_templates($user2)));
 	}
 }
