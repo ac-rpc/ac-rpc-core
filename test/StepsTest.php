348f70051d3302ac30b76ca67dec2475683f5032
Michael Berkowski <mjb@umn.edu>
2014-12-11 21:32:48 -0600

    A few tests for step manipulation

--- test/StepsTest.php
+++ test/StepsTest.php
@@ -20,9 +20,30 @@ class RPC_StepsTest extends RPC_PHPUnit_Extensions_Databaase_TestCase
 		$this->config = RPC_Config::get_instance(__DIR__ . '/fixtures/config.inc.php');
 		$this->db = RPC_DB::get_connection($this->config);
 	}
-	public function testUpdateStep()
+	public function testUpdateStepValidUser()
 	{
-		
+		$user1 = new RPC_User(RPC_User::RPC_QUERY_USER_BY_ID, 1, $this->config, $this->db);
+		$step1 = new RPC_Step(1, $user1, $this->config, $this->db);
+
+		// Step was created
+		$this->assertEquals('User 1 Assignment 1 Step 1', $step1->title);
+
+		// Change a property and save
+		$new_title = 'Updated Title';
+		$step1->title = $new_title;
+		$step1->update();
+		$this->assertEquals($new_title, $step1->title);
+
+	}
+	public function testUpdateStepInvalidUser()
+	{
+		$user2 = new RPC_User(RPC_User::RPC_QUERY_USER_BY_ID, 2, $this->config, $this->db);
+		$step1 = new RPC_Step(1, $user2, $this->config, $this->db);
+
+		$new_title = 'Updated Title';
+		$step1->title = $new_title;
+		$this->assertFalse($step1->update());
+		$this->assertEquals(RPC_Step::ERR_ACCESS_DENIED, $step1->error);
 	}
 }
 ?>
